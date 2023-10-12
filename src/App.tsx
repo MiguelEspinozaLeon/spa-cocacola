ea2dcbd6f54302f5bcb35ce31b707ab7886e496b
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2023-10-12 16:03:48 -0700

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {HashLink as Link} from 'react-router-hash-link'
 import AboutPage from './sections/AboutPage'
 import ContactPage from './sections/ContactPage'
 import ProductsPage from './sections/ProductsPage'
-import NavBar from './layout/NavBar'
+
 
 function App() {
 
@@ -12,21 +12,21 @@ function App() {
     <>
       <BrowserRouter>
       
-      <nav className='relative bg-gray-700 border-gray-200 dark:bg-gray-900'>
-        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto absolute inset-x-0 top-0'>
-          <a href="https://www.coca-cola.com.mx/" className='flex items-center'>
+      <nav className='relative z-10'>
+        <div className='fixed w-full bg-sky-950 p-2 flex flex-wrap items-center justify-between mx-auto inset-x-0 top-0 '>
+          <a href="https://www.google.com" className='flex items-center'>
             <img className='h-20 mr-3' src="https://placehold.co/600x400" alt="" />
-            <span className='self-center text-3xl font-semibold text-slate-700 hover:text-blue-600'>Empresa</span>
+            <span className='self-center text-3xl font-semibold text-stone-50'>Empresa</span>
           </a>
-          <ul className='flex flex-row justify-start gap-4 text-xl'>
+          <ul className='flex flex-row justify-start gap-4 text-xl text-stone-50 px-4'>
             <li>
-              <a href="#">Home</a>
+              <a className='hover:font-bold' href="#">Home</a>
             </li>
             <li>
-              <Link to="#about-page">About</Link>
+              <Link className='hover:font-bold' to="#about-page">About</Link>
             </li>
             <li>
-              <Link to="#contact-page">Contact</Link>
+              <Link className='hover:font-bold' to="#contact-page">Contact</Link>
             </li>
             
           </ul>
