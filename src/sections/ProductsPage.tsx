dc4ceb3971218f4de93d5b335f8e8d19815eaff5
miguel.espinozaleon <miguel.espinozaleon@gmail.com>
2023-10-13 17:25:50 -0700

    x

--- src/sections/ProductsPage.tsx
+++ src/sections/ProductsPage.tsx
@@ -29,11 +29,9 @@ export default function ProductsPage(){
     }, [currentSlide]);
     return (
         <section id="products-page">
-            
             <h2 className="text-2xl">These are our products.</h2>
             <div className="max-w-[1400px] h-[600px] w-full m-auto py-16 px-4 relative group">
                 <div style={{backgroundImage: `url(${slides[currentSlide].url})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
-
                 <div className="hidden absolute group-hover:block top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer" onClick={prevSlide}>
                     <BsChevronCompactLeft size={30}/>
                 </div>
